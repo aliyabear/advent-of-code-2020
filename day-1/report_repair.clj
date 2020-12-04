613a9d77b9cee821fa3d976ff27189095b9c75c2
Aliya Hassam <aliya.hassam@unbounce.com>
2020-12-03 23:10:18 -0800

    Day 1: part deux

--- day-1/report_repair.clj
+++ day-1/report_repair.clj
@@ -13,21 +13,31 @@
 ;; In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
 ;;
 ;; Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
-
-
-(defn repair-report [input]
+(defn sum-product [input sum]
   (let [find-entries (fn[input curr]
-                       (let [diff (- 2020 curr)]
+                       (let [diff (- sum curr)]
                          (if (some #(= diff %) input)
                            (* diff curr)
                            (recur (drop 1 input) (second input)))))]
 
     (find-entries input (first input))))
 
+;; Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+;;
+;; In your expense report, what is the product of the three entries that sum to 2020?
+(defn triplet-product [input sum]
+  (doseq [meow input]
+    (doseq [fox (drop 1 input)]
+      (doseq [rulez (drop 2 input)]
+        (when (= sum (+ meow fox rulez))
+          (println (* meow fox rulez)))))))
+
+
 (comment
 
   (def input [123 1721 979 366 299 675 1456])
 
-  (repair-report input)
+  (sum-product input 2020)
+  (triplet-product input 2020)
   )
 
