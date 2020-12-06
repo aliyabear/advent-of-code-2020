92eadd39901a46a302675e9cf67e2116c85f9e64
aliyabear <aliya.hassam@gmail.com>
2020-12-06 00:18:52 -0800

    Day 5: sad attempt at cleaning stuff up

--- day-5/binary_boarding.clj
+++ day-5/binary_boarding.clj
@@ -1,14 +1,15 @@
-
-(require '[clojure.string :as str])
+(ns advent-of-code.day-5
+  (:require '[clojure.string :as str]))
 
 (comment
   ;; let's hardcode some defaults
   (def seats 127)
   (def columns 7)
   (def input "BBFFBBFRLL")
-  (take 7 input)
 
 
+  ;; Let's just leave everything in the comment section!
+  ;; that's how hacky we feel today!
   (defn get-row [input x y low]
     (let [current (first input)
           z  (+ x(quot (- y x) 2))
@@ -26,21 +27,11 @@
             (recur (rest input) lower upper low))))
 
 
-(+ (*(get-row (take 7 input) 0 seats \F) 8) (get-row (drop 7 input) 0 columns \L))
+(+ (* (get-row (take 7 input) 0 seats \F) 8) (get-row (drop 7 input) 0 columns \L))
 
 (def real-input (str/split (slurp "input.txt") #"\n"))
 
 (map #( (+ (* (get-row (take 7 %) 0 seats \F) 8)
            (get-row (drop 7 %) 0 columns \L)) ) real-input )
 
-(def blah (sort (map (fn [i] 
-                       (+ (*(get-row (take 7 i) 0 seats \F) 8) (get-row (drop 7 i) 0 columns \L))) real-input)))
-
-(def n(count blah))
-(- sum (quot (* n (+ 1 n)) 2))
-(def sum (reduce + blah))
-
-(def total (*(+ (count blah) 1) (quot (count blah) 2)) )
-
-(- sum total)
 _)
