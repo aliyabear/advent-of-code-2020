e6996c0edc4292519257a1c5226f81358ae2fda8
aliyabear <aliya.hassam@gmail.com>
2020-12-06 00:15:57 -0800

    Day 6: Part Two

--- day-6/custom_customs.clj
+++ day-6/custom_customs.clj
@@ -10,28 +10,28 @@
 (defn sum-of-counts [input]
   (reduce + (map (fn [i] (count (frequencies i))) (parse-input input))))
 
+(defn partition-input "For Part Two" [input] 
+  (filter (fn [i] (not (empty? (first i)))) (partition-by (partial empty?) (str/split input #"\n"))))
+
+(defn everyone-says-yes [input]
+  ;; I'm so hacky.. oh so hacky and happy and freeeeeee!
+  (quot (count (flatten (map (fn [i]
+                               (let [num-passengers (count i)
+                                     freq (frequencies(str/join #"" i))]
+                                 (filter (fn [[k v]] (= num-passengers v) )freq))) (partition-input input))))
+        ;; temp hack to divide by 2 as flatten will return a sequence like this (\a 1 \b 2 \c 3)
+        2))
+
 (comment
 
+  (def input (slurp "sample-input.txt"))
+  (def real-input-part-2 (slurp "input.txt"))
 
-  (def input 
-    ["abc"
-     ""
-     "a"
-     "b"
-     "c"
-     ""
-     "ab"
-     "ac"
-     ""
-     "a"
-     "a"
-     "a"
-     "a"
-     ""
-     "b"])
+  (everyone-says-yes input)
+  (everyone-says-yes real-input-part-2)
 
-  (sum-of-counts input)
-
+  (sum-of-counts (str/split input #"\n"))
+  
   (def real-input (str/split (slurp "input.txt") #"\n"))
 
   (sum-of-counts real-input)
